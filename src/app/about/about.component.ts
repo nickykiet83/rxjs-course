ab12b6c5e102f13870be5e0f2ca173aee155f4d4
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2019-01-13 22:24:57 +1100

    interval, timer, fromEvent

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { interval, timer, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,20 +11,24 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    document.addEventListener('click', evt => {
-      console.log(evt);
-    });
+    // ex1: interval: Creates an Observable that emits sequential numbers every specified interval of time, on a specified SchedulerLike.
+    // const interval$ = interval(1000);
 
-    let counter = 0;
-    setInterval(() => {
-      console.log(counter);
-      counter++;
-    }, 1000);
+    // interval$.subscribe(val => console.log(`stream 1 => ${val}`));
 
+    // interval$.subscribe(val => console.log(`stream 2 => ${val}`));
 
-    setTimeout(() => {
-      console.log('finished...');
-    }, 3000);
+    // ------------
+
+    // ex2: timer : Creates an Observable that starts emitting after an dueTime and emits ever increasing numbers after each period of time thereafter.
+    const interval$ = timer(3000, 1000);
+
+    interval$.subscribe(val => console.log(`stream 1 => ${val}`));
+
+    // ex3: fromEvent: Creates an Observable that emits events of a specific type coming from the given event target.
+    const click$ = fromEvent(document, 'click');
+
+    click$.subscribe(evt => console.log(evt));
 
   }
 
