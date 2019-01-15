378fa1cf09eb985be02547acc834fa91e298e4af
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2019-01-15 16:34:11 +1100

    Finishing the Search Typeahead - The switchMap Operator

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -16,6 +16,7 @@ import {
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
+import { searchLessons } from '../../../server/search-lessons.route';
 
 
 @Component({
@@ -24,7 +25,7 @@ import { createHttpObservable } from '../common/util';
     styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit, AfterViewInit {
-
+    courseId: string;
     course$: Observable<Course>;
 
     lessons$: Observable<Lesson[]>;
@@ -38,11 +39,14 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
 
-        const courseId = this.route.snapshot.params['id'];
+        this.courseId = this.route.snapshot.params['id'];
 
-        this.course$ = createHttpObservable(`/api/courses/${courseId}`);
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
 
-        this.lessons$ = createHttpObservable(`/api/lessons?courseId=${courseId}&pageSize=100`)
+    }
+
+    loadLesson(search = ''): Observable<Lesson[]> {
+        return createHttpObservable(`/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`)
             .pipe(
                 map(res =>  res['payload'])
             );
@@ -50,17 +54,16 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngAfterViewInit() {
 
-        fromEvent<any>(this.input.nativeElement, 'keyup')
+        const searchLesson$ = fromEvent<any>(this.input.nativeElement, 'keyup')
             .pipe(
                 map(event => event.target.value),
                 debounceTime(400),
-                distinctUntilChanged()
-            )
-            .subscribe(console.log);
+                distinctUntilChanged(),
+                switchMap(search => this.loadLesson(search))
+            );
 
+        const initialLesson$ = this.loadLesson();
+        this.lessons$ = concat(initialLesson$, searchLesson$);
     }
-
-
-
 
 }
