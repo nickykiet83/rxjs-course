aa543e464dad7633b4aa33e5e75f7f22287fe446
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2019-01-16 00:45:17 +1100

    The RxJs Debug Operator - Implementation Conclusion

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,3 +1,4 @@
+import { RxJsLoggingLevel, setRxJsLoggingLevel } from './../common/debug';
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Course} from "../model/course";
@@ -17,6 +18,7 @@ import {merge, fromEvent, Observable, concat, interval} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
 import { searchLessons } from '../../../server/search-lessons.route';
+import { debug } from '../common/debug';
 
 
 @Component({
@@ -41,8 +43,12 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
         this.courseId = this.route.snapshot.params['id'];
 
-        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`)
+            .pipe(
+                debug( RxJsLoggingLevel.INFO, 'course value'),
+            );
 
+        setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
     }
 
     loadLesson(search = ''): Observable<Lesson[]> {
@@ -53,12 +59,16 @@ export class CourseComponent implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit() {
-        fromEvent<any>(this.input.nativeElement, 'keyup')
+        this.lessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
             .pipe(
                 map(event => event.target.value),
-                throttleTime(500)
-            )
-            .subscribe(console.log);
+                startWith(''),
+                debug( RxJsLoggingLevel.TRACE, 'search'),
+                debounceTime(400),
+                distinctUntilChanged(),
+                switchMap(search => this.loadLesson(search)),
+                debug( RxJsLoggingLevel.DEBUG, 'lessons value'),
+            );
     }
 
 }
