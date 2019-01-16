fa1524fb193c10280d2a28a974af3aa8c08f4bd6
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2019-01-16 12:09:32 +1100

    rsjs subject

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -12,8 +12,22 @@ import {createHttpObservable} from '../common/util';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
+      const subject = new Subject();
 
+      const series$ = subject.asObservable();
 
+      series$.subscribe(val => console.log('early sub: ' + val));
+
+      subject.next(1);
+      subject.next(2);
+      subject.next(3);
+      // subject.complete();
+
+      setTimeout(() => {
+        series$.subscribe(val => console.log('late sub: ' + val));
+
+        subject.next(4);
+      }, 3000);
     }
 
 
